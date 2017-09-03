7dbfd5eacf7d90e7b19a20ce97be227e11876a4c
Omar Kassouar <omar@kassouar.com>
2017-09-03 02:35:40 -0400

    let homebrew install xcode clt on its own

--- homebrew.rb
+++ homebrew.rb
@@ -2,24 +2,16 @@ require 'tty'
 
 cmd = TTY::Command.new
 
-if cmd.run('xcode-select -p').failure?
-  # trick macOS updater into thinking XCODE COMMAND LINE TOOLS are available
-  cmd.run('touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress')
-  # install XCODE COMMAND LINE TOOLS and other pending updates
-  cmd.run('softwareupdate -i -a')
-end
+# install homebrew (also installs Xcode CLTools)
+cmd.run '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install < /dev/null)"'
 
-# install homebrew
-cmd.run('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install < /dev/null)"')
+cmd.run <<~EOS
+  brew analytics off && \
+  brew doctor && \
+  brew update && \
+  brew tap caskroom/cask && \
+  brew cask doctor
+EOS
 
-# this may no longer be necessary
-# cmd.run('chown -R "$(whoami)" /usr/local') # fixes occasional permissions issue
-
-cmd.run('brew analytics off')
-cmd.run('brew doctor')
-cmd.run('brew update')
-cmd.run('brew tap caskroom/cask ')
-cmd.run('brew cask doctor')
-cmd.run('brew bundle')
-cmd.run('brew cleanup -s')
-cmd.run('brew cask cleanup')
+cmd.run 'brew bundle'
+cmd.run 'brew cleanup -s && brew cask cleanup'
