4e25b0ad36b0e85a757a53739ccc4229f46361b3
Omar Kassouar <omar@kassouar.com>
2017-09-03 22:37:11 -0400

    cant use new heredoc syntax because default ruby on mac is ancient

--- lib/homebrew.rb
+++ lib/homebrew.rb
@@ -5,12 +5,12 @@ cmd = TTY::Command.new
 # install homebrew (also installs Xcode CLTools)
 cmd.run '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install < /dev/null)"'
 
-cmd.run <<~EOS
-  brew analytics off && \
-  brew doctor && \
-  brew update && \
-  brew tap caskroom/cask && \
-  brew cask doctor
+cmd.run <<EOS
+brew analytics off && \
+brew doctor && \
+brew update && \
+brew tap caskroom/cask && \
+brew cask doctor
 EOS
 
 cmd.run 'brew bundle'
