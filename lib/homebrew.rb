92ee0e1f3865cdb603352f5bee25eeb0a1a14ade
Omar Kassouar <omar@kassouar.com>
2017-09-05 02:06:49 -0400

    dont reinstall brew if already exists

--- lib/homebrew.rb
+++ lib/homebrew.rb
@@ -3,14 +3,16 @@ require 'tty'
 cmd = TTY::Command.new
 
 # install homebrew (also installs Xcode CLTools)
-cmd.run '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install < /dev/null)"'
+if cmd.run!('which brew').failure?
+  cmd.run '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install < /dev/null)"'
+end
 
 cmd.run <<EOS
-brew analytics off && \
-brew doctor && \
-brew update && \
-brew tap caskroom/cask && \
-brew cask doctor
+  brew analytics off && \
+  brew doctor && \
+  brew update && \
+  brew tap caskroom/cask && \
+  brew cask doctor
 EOS
 
 cmd.run 'brew bundle'
